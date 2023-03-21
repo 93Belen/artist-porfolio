33aabd8c97f4caecf8e7a6aabee64a557905c16c
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-21 18:10:03 -0500

    added pic to home page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -28,6 +28,7 @@ export default function About() {
       ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
       sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <Image width={12000} className='rounded-xl self-center justify-self-center m-auto' src={require('../../../imgs/landscape.jpg')} />
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -15,28 +15,21 @@ export default function Home() {
 
 
   return (
-    <div className='w-screen md:p-8'>
-    <div className='grid min-h-screen gap-9 md:grid-cols-3 md:grid-rows-2 p-7 justify-items-center w-screen md:w-3/4 justify-self-center m-auto'>
-      <Image id='pic-one' onClick={openModal} className='h-auto w-full md:w-auto md:h-64 rounded-md md:hover:rotate-[-10deg] duration-500 cursor-pointer]' src={require('../../imgs/one.jpg')} />
+    <div className='w-screen md:p-6 h-fit md:h-screen'>
+        <Image className='fixed hidden md:block left-0 md:mt-60 md:mb-[-250px] bottom-0' src={require('../../imgs/nature.png')} />
+    <div className=' h-fit md:h-screen'>
+      <p className='md:w-11/12 justify-self-center p-8 text-3xl md:text-4xl m-auto md:p-12'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate </p>
+    <div className='grid gap-9 md:grid-cols-3 md:grid-rows-2 p-7 justify-items-center w-screen md:w-3/4 justify-self-center m-auto'>
+      <Image onClick={openModal} className='h-auto w-full md:w-auto md:h-64 rounded-md md:hover:rotate-[-10deg] duration-500 cursor-pointer]' src={require('../../imgs/one.jpg')} />
       <Image onClick={openModal} className='h-auto w-full md:w-auto md:h-64 rounded-md md:hover:rotate-[-10deg] duration-500 cursor-pointer' src={require('../../imgs/two.jpg')} />
       <Image onClick={openModal} className='h-auto w-full md:w-auto md:h-64 rounded-md md:hover:rotate-[-10deg] duration-500 cursor-pointer' src={require('../../imgs/three.jpg')} />
 
-      <Image onClick={openModal} className='h-auto w-full md:w-auto md:h-64 rounded-md md:hover:rotate-[-10deg] duration-500 cursor-pointer' src={require('../../imgs/four.jpg')} />
-      <Image onClick={openModal} className='h-auto w-full md:w-auto md:h-64 rounded-md md:hover:rotate-[-10deg] duration-500 cursor-pointer' src={require('../../imgs/five.jpg')} />
-      <Image onClick={openModal} className='h-auto w-full md:w-auto md:h-64 rounded-md md:hover:rotate-[-10deg] duration-500 cursor-pointer' src={require('../../imgs/six.jpg')} />   
-      <Link className='justify-self-end md:justify-self-center md:col-start-3 hover:text-orange-400' href='/work'>See more...</Link>   
+      <Link className='justify-self-end md:justify-self-center md:col-start-3 hover:text-orange-400 mt-0 pt-0' href='/work'>See more...</Link>  
     </div>
-    <p className='md:w-11/12 justify-self-center p-8 text-3xl md:text-2xl m-auto md:p-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-      sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
     </div>
   )
 }
