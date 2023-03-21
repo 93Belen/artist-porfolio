8996d8a15ae27523c9b7cfd53ffc20e56fd10894
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-21 05:56:29 -0500

    redux seems to be working

--- src/app/modal.js
+++ src/app/modal.js
@@ -1,7 +1,24 @@
+"use client"
+
+import { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { selectShowModal } from "../../redux/selectors"
+
+
 
 export default function Modal(){
+    const showModal = useSelector(selectShowModal)
+    useEffect(()=> {
+        if(showModal){
+            document.getElementById('modal').style.display = 'block'
+        }
+        else{
+            document.getElementById('modal').style.display = 'none'
+        }
+    })
+
     return (
-        <div className='hidden md:block fixed m-auto w-4/5 h-4/5 bg-white rounded-2xl left-36 z-10'>
+        <div id='modal' className='fixed m-auto w-4/5 h-4/5 bg-white rounded-2xl left-36 z-10'>
             <p>X</p>
         </div>
     )
