dd530c04c72f7254134115589e0b439a8b419440
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-21 16:30:02 -0500

    basic about content

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,8 +6,28 @@ import Image from "next/image";
 export default function About() {
 
   return (
-    <div className='w-screen md:p-8'>
-    <p>about</p>
+    <div className='w-screen md:p-12 sm:p-4 grid md:grid-cols-[65%_35%] md:grid-rows-2 box-border'>
+    <p className='text-3xl'>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <Image height={300} className='rounded-xl justify-self-center self-center m-4 md:m-5' src={require('../../../imgs/seven.jpg')} />
+      <p className='md:col-span-2 text-3xl'>ed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
   )
 }
