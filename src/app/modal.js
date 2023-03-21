e918253838d7a8eeeb928dc78b6fa476557db8b8
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-21 12:20:30 -0500

    set modal in absolute, fixed gives to many problems

--- src/app/modal.js
+++ src/app/modal.js
@@ -25,7 +25,7 @@ export default function Modal(){
     }
 
     return (
-        <div id='modal' className='h-screen w-screen fixed z-10  md:rounded-xl hidden'>
+        <div id='modal' className='h-[80vh] w-screen absolute top-[500px] z-10  md:rounded-xl hidden'>
             <div className='grid p-8 bg-white min-h-fit sm:w-full md:grid-cols-2 md:grid-rows-[5%_60%_35%] gap-4 md:w-4/5 m-auto md:rounded-xl'>
                 <button onClick={closeModal} className='text-black md:col-start-2 md:self-start md:justify-self-end md:hover:rotate-180'>X</button>
                 <div className='p-2 h-fit justify-items-center items-center'><Image className='h-auto w-full md:w-auto md:h-64 m-auto rounded-md md:rotate-[-10deg]' src={require('../../imgs/one.jpg')} /></div>
@@ -36,8 +36,13 @@ export default function Modal(){
                     <p>date</p>
                     <p>something else</p>
                 </div>
-                <div className='text-black md:col-span-2 p-4 w-auto'>
+                <div className='text-black md:col-span-2 p-12 sm:p-4 w-auto'>
                     <p>ore veritatis et quasi architecto beatae vitae dicta sunt expli
+                        cabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut o
+                        dit aut fugit, sed quia consequuntur magni dolores eos qui ratione v
+                        oluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsu
+                        error sit voluptatem accusantium doloremque laud
+                        ore veritatis et quasi architecto beatae vitae dicta sunt expli
                         cabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut o
                         dit aut fugit, sed quia consequuntur magni dolores eos qui ratione v
                         oluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsu
