10efa2fb0f767a76b08b1e48ff2907b6d9dbe86a
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-21 14:22:44 -0500

    better modal background

--- src/app/modal.js
+++ src/app/modal.js
@@ -20,22 +20,22 @@ export default function Modal(){
     }
 
     return (
-        <div id='modal' className='h-[80vh] w-screen absolute z-10  md:rounded-xl hidden'>
-            <div className='grid p-8 min-h-fit sm:w-full md:grid-cols-2 md:grid-rows-[5%_60%_35%] gap-4 md:w-4/5 m-auto md:rounded-xl bg-white'>
-                <button onClick={closeModal} className='text-black md:col-start-2 md:self-start md:justify-self-end'><svg id='close-modal-svg' width="25" height="100%" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div id='modal' className='h-[90vh] w-screen absolute z-10  md:rounded-xl hidden'>
+            <div className='grid p-8 min-h-fit sm:w-full md:grid-cols-2 md:grid-rows-[5%_60%_35%] gap-4 md:w-4/5 m-auto md:rounded-xl backdrop-blur-2xl'>
+                <button onClick={closeModal} className='text-white md:col-start-2 md:self-start md:justify-self-end'><svg id='close-modal-svg' width="25" height="100%" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <line x1="1.5" y1="-1.5" x2="23.5" y2="-1.5" transform="matrix(-0.642788 0.766044 0.766044 0.642788 18.5348 2.42444)" stroke="#FB923C" stroke-width="3" stroke-linecap="round"/>
                     <line x1="17.4215" y1="19.3907" x2="3.28021" y2="2.5377" stroke="#FB923C" stroke-width="3" stroke-linecap="round"/>
                 </svg>
 </button>
                 <div className='p-2 h-fit justify-items-center items-center'><Image className='h-auto w-full md:w-auto md:h-64 m-auto rounded-md md:rotate-[-10deg]' src={require('../../imgs/one.jpg')} /></div>
-                <div className='text-black p-4'>
+                <div className='text-white p-4'>
                     <p>titulo</p>
                     <p>precio</p>
                     <p>id number</p>
                     <p>date</p>
                     <p>something else</p>
                 </div>
-                <div className='text-black md:col-span-2 p-12 sm:p-4 w-auto'>
+                <div className='text-white md:col-span-2 p-12 sm:p-4 w-auto'>
                     <p>ore veritatis et quasi architecto beatae vitae dicta sunt expli
                         cabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut o
                         dit aut fugit, sed quia consequuntur magni dolores eos qui ratione v
