df5554060cc070b32990fc3a31500cd07e01fa6e
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-22 07:17:12 -0500

    about page done for now

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -15,11 +15,16 @@ export default function About() {
       let windowheight = window.innerHeight;
       let revealtop = reveals[i].getBoundingClientRect().top;
       let revealpoint = 50;
-
       if(revealtop < windowheight - revealpoint){
-        document.getElementById(`${reveals[i].id}`).style.transform = 'translateY(0px)'
-        document.getElementById(`${reveals[i].id}`).style.opacity = '1';
-        document.getElementById(`${reveals[i].id}`).style.transitionDuration = '2s'
+        if(reveals[i].id === 'nine-element'){
+          document.getElementById(`${reveals[i].id}`).style.opacity = '1';
+          document.getElementById(`${reveals[i].id}`).style.transitionDuration = '2s'
+        }
+        else {
+          document.getElementById(`${reveals[i].id}`).style.transform = 'translateY(0px)'
+          document.getElementById(`${reveals[i].id}`).style.opacity = '1';
+          document.getElementById(`${reveals[i].id}`).style.transitionDuration = '2s'
+        }
       }
       else{
         document.getElementById(`${reveals[i].id}`).style.transform = 'translateY(50px)'
@@ -36,8 +41,8 @@ export default function About() {
   window.addEventListener('scroll', reveal);
 
   return (
-    <div className='w-screen md:p-12 sm:p-4 grid md:grid-cols-[65%_35%] md:grid-rows-2 box-border'>
-    <p id='second-element' className='reveal text-3xl'>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    <div className='w-screen grid md:grid-cols-[55%_45%] md:grid-rows-[repeat(6, fit-content)] box-border gap-6 md:gap-4 justify-items-center h-fit'>
+    <p id='second-element' className='reveal text-3xl m-12 md:p-10'>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
       ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
@@ -47,8 +52,8 @@ export default function About() {
       ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
       sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <Image id='third-element' height={300} className=' reveal rounded-xl justify-self-center self-center m-4 md:m-5' src={require('../../../imgs/seven.jpg')} />
-      <p id='fourth-element' className='reveal md:col-span-2 text-3xl'>ed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      <Image id='third-element' height={500} className=' reveal rounded-xl justify-self-center self-center m-4 md:m-5' src={require('../../../imgs/seven.jpg')} />
+      <p id='fourth-element' className='reveal md:col-span-2 text-3xl m-12 md:p-10'>ed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
       ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
@@ -58,8 +63,8 @@ export default function About() {
       ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
       sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <Image id='fith-element' width={12000} className='reveal rounded-xl self-center justify-self-center m-auto' src={require('../../../imgs/landscape.jpg')} />
-      <p id='six-element' className='reveal text-3xl'>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      <Image id='fith-element' width={2000} className='reveal rounded-xl self-center justify-self-center m-auto md:col-span-2' src={require('../../../imgs/mountains-2.png')} />
+      <p id='six-element' className='reveal text-3xl m-12 md:col-span-2 md:p-10'>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
       ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
@@ -69,8 +74,8 @@ export default function About() {
       ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
       sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <Image id='seven-element' height={300} className='reveal rounded-xl justify-self-center self-center m-4 md:m-5' src={require('../../../imgs/seven.jpg')} />
-      <p id='eight-element' className='reveal md:col-span-2 text-3xl'>ed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      <Image id='seven-element' height={500} className='reveal rounded-xl justify-self-center self-center m-4 md:m-5' src={require('../../../imgs/three.jpg')} />
+      <p id='eight-element' className='reveal text-3xl m-12 md:p-10'>ed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
       ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
@@ -80,7 +85,17 @@ export default function About() {
       ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
       sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <Image id='nine-element' width={12000} className='reveal rounded-xl self-center justify-self-center m-auto' src={require('../../../imgs/landscape.jpg')} />
+      <p id='ten-element' className='reveal text-3xl m-12 md:col-span-2 md:p-10'>ed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+      ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+      sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <Image id='nine-element' width={12000} className='reveal rounded-xl self-center justify-self-center md:col-span-2 p-0 m-auto' src={require('../../../imgs/trees.png')} />
     </div>
   )
 }
