e9ffcc6e50dd6b767997ca55da1d003f69354171
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-23 19:06:25 -0500

    shows page in work with background color change

--- src/app/work/layout.js
+++ src/app/work/layout.js
@@ -1,28 +1,53 @@
+'use client';
 
+import { usePathname } from 'next/navigation';
 import '../globals.css'
 import Link from 'next/link'
 
 
+
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+  console.log(pathname);
 
   return (
     <div className='items-center justify-center justify-items-center w-full'>
         <nav className='m-auto self-center justify-self-center flex'>
             <ul class="inline-flex items-center -space-x-px self-center justify-self-center m-auto">
                 <li>
-                <Link href="/work" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white active:bg-orange-400">1</Link>
+                <Link href="/work" className={`px-3 py-2 leading-tight text-gray-500  border border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white active:bg-orange-400 ${
+                  pathname === '/work'
+                  ? "bg-teal-800"
+                  : "dark:bg-gray-800"
+                }`}>1</Link>
                 </li>
                 <li>
-                <Link href="/work/two" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</Link>
+                <Link href="/work/two" className={`px-3 py-2 leading-tight text-gray-500  border border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white active:bg-orange-400 ${
+                  pathname === '/work/two'
+                  ? "bg-teal-800"
+                  : "dark:bg-gray-800"
+                }`}>2</Link>
                 </li>
                 <li>
-                <Link href="/work/three" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">3</Link>
+                <Link href="/work/three" className={`px-3 py-2 leading-tight text-gray-500  border border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white active:bg-orange-400 ${
+                  pathname === '/work/three'
+                  ? "bg-teal-800"
+                  : "dark:bg-gray-800"
+                }`}>3</Link>
                 </li>
                 <li>
-                <Link href="/work/four" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</Link>
+                <Link href="/work/four" className={`px-3 py-2 leading-tight text-gray-500  border border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white active:bg-orange-400 ${
+                  pathname === '/work/four'
+                  ? "bg-teal-800"
+                  : "dark:bg-gray-800"
+                }`}>4</Link>
                 </li>
                 <li>
-                <Link href="/work/five" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</Link>
+                <Link href="/work/five" className={`px-3 py-2 leading-tight text-gray-500  border border-gray-300 hover:bg-gray-100 hover:text-gray-700  dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white active:bg-orange-400 ${
+                  pathname === '/work/five'
+                  ? "bg-teal-800"
+                  : "dark:bg-gray-800"
+                }`}>5</Link>
                 </li>
             </ul>
         </nav>
