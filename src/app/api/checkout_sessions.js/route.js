4115e3316e0fbf4651b745b8d5e5b427ff81ba45
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-26 10:18:23 -0500

    limit item from stripe to 60

--- src/app/api/checkout_sessions.js/route.js
+++
@@ -1,4 +0,0 @@
-import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
--- src/app/api/items/route.js
+++ src/app/api/items/route.js
@@ -3,7 +3,9 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 
 export async function GET(request) {
-    const items = await stripe.products.list({});
+    const items = await stripe.products.list({
+      limit: 60
+    });
     return new Response(JSON.stringify(items))
   }
 
