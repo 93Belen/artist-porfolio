0c318c95e2a720bafe1953baf456b98f560bcc1a
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-03-27 08:09:45 -0500

    styling success page

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -7,8 +7,10 @@
 export default function Home() {
 
   return (
-    <div className='w-screen md:p-6 h-fit md:h-screen' >
-        <p>Thanks you for buying!</p>
+    <div className='md:p-6 h-fit md:h-screen w-screen text-center p-4 self-center justify-self-center' >
+        <p className='text-lg'>Thanks you for buying and supporting art!<br/>
+        We have sent you an email with your purchase's information
+        </p>
     </div>
   )
 }
